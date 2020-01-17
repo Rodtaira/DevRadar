51ac81c99548c8421af0d757f57b23df7d985ed0
rodtaira-movecode <rodrigotaira@movecode.com>
2020-01-17 16:04:57 -0300

    adding Axios modules to get github user information

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,27 @@
 const {Router} = require('express')
 const axios    = require('axios')
+const Dev      = require('./models/Developer')
 const routes = Router()
 
 routes.post('/devs', async (req, res) => {
 
-    const{github_username} = req.body
+    const{github_username, techs, latitude, longitude} = req.body
     
      const apiResponse  = await axios.get(`https://api.github.com/users/${github_username}`)
-     console.log(apiResponse.data)
+     
+     const {name = login, avatar_url, bio } = apiResponse.data
 
-    return res.json({message: "Popo"})
+     const techsArray = techs.split(',').map(tech => tech.trim())
+     
+     const developer = await Dev.create({
+         github_username, 
+         name, 
+         avatar_url, 
+         bio, 
+         techs: techsArray, 
+     })
+
+    return res.json(developer)
 })
 
 module.exports = routes
