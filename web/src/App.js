c48d23a330ce34d9107a04865eee802e5c7a4ca4
rodtaira-movecode <rodrigotaira@movecode.com>
2020-01-20 13:33:33 -0300

    First styles in css

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './global.css'
+import './App.css'
 
 function App() {
   return (
     <div id="app">
       <aside>
+        <strong > Register</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">GitHub Username</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+          
+          <div className="input-block">
+            <label htmlFor="techs">Technologies</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div>
+            <label htmlFor="techs">Technologies</label>
+            <input name="techs" id="techs" required/>
+          </div>          
+        </form>
       </aside>
       <main>
       </main>  
